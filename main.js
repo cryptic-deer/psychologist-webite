07c71e6cbfc0ecf99a92f0058c40461c9ca398ab
cryptic-deer <artist.tibi@gmail.com>
2022-07-22 23:33:36 +0200

    Complete first iteration

---
+++ main.js
@@ -0,0 +1,9 @@
+const navLinks = document.querySelectorAll("#navLink");
+
+for (const navLink of navLinks) {
+	navLink.addEventListener("click", () => {
+		navLinks.forEach(navLink => navLink.classList.remove("active"));
+		document.getElementById("checkbox").checked = false;
+		navLink.classList.add("active");
+	});
+}
